de21bf046ec2a5e4a6d856d77c12a90900dd08c2
Kybrnaut <117260821+Kybrnaut@users.noreply.github.com>
2023-02-07 00:36:15 -0600

    Add files via upload

--- 1partitions.sh
+++ 1partitions.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-wipefs -fa /dev/nvme0n1;parted /dev/nvme0n1 mklabel gpt;parted /dev/nvme0n1 mkpart '"raid"' 1% 100%;parted /dev/nvme0n1 set 1 raid on;parted /dev/nvme0n1 align-check optimal 1;gdisk -l /dev/nvme0n1; mdadm --misc --zero-superblock /dev/nvme0n1
+wipefs -fa /dev/nvme0n1;parted /dev/nvme0n1 mklabel gpt;parted /dev/nvme0n1 mkpart "raid" 1% 100% && parted /dev/nvme0n1 set 1 raid on;parted /dev/nvme0n1 align-check optimal 1;gdisk -l /dev/nvme0n1; mdadm --misc --zero-superblock /dev/nvme0n1
 
-wipefs -fa /dev/nvme1n1;parted /dev/nvme1n1 mklabel gpt;parted /dev/nvme1n1 mkpart '"raid"' 1% 100%;parted /dev/nvme1n1 set 1 raid on;parted /dev/nvme1n1 align-check optimal 1;gdisk -l /dev/nvme1n1; mdadm --misc --zero-superblock /dev/nvme1n1
+wipefs -fa /dev/nvme1n1;parted /dev/nvme1n1 mklabel gpt;parted /dev/nvme1n1 mkpart "raid" 1% 100% && parted /dev/nvme1n1 set 1 raid on;parted /dev/nvme1n1 align-check optimal 1;gdisk -l /dev/nvme1n1; mdadm --misc --zero-superblock /dev/nvme1n1
 
-wipefs -fa /dev/nvme4n1;parted /dev/nvme4n1 mklabel gpt;parted /dev/nvme4n1 mkpart '"raid"' 1% 100%;parted /dev/nvme4n1 set 1 raid on;parted /dev/nvme4n1 align-check optimal 1;gdisk -l /dev/nvme4n1; mdadm --misc --zero-superblock /dev/nvme4n1
+wipefs -fa /dev/nvme4n1;parted /dev/nvme4n1 mklabel gpt;parted /dev/nvme4n1 mkpart "xfs" 1% 100% && parted /dev/nvme4n1 set 1 raid on;parted /dev/nvme4n1 align-check optimal 1;gdisk -l /dev/nvme4n1; mdadm --misc --zero-superblock /dev/nvme4n1
--- 2raid0xfs.sh
+++ 2raid0xfs.sh
@@ -1,10 +1,5 @@
 #!/bin/bash
 
 mdadm --create --verbose /dev/md0 --chunk=128 --level=0 --raid-devices=3 --name=md0 --homehost=any /dev/nvme0n1p1 /dev/nvme1n1p1 /dev/nvme4n1p1
-mdadm --detail /dev/md0
-
-
-#Good2Go-a?
-mkfs.xfs -L xfs0 -d bs=4096 -l lazy-count=1 -l version=2,su=128k -isize=512 -d agcount=32 -m rmapbt=1 /dev/md0
-mdadm --detail --scan >> /etc/mdadm.conf
+mdadm --detail /dev/md0 && mkfs.xfs -L xfs0 -l lazy-count=1 -l version=2,su=128k -isize=512 -d agcount=32 -m rmapbt=1 /dev/md0 && mdadm --detail --scan >> /etc/mdadm.conf
 
